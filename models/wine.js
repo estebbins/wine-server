1a1e528b164c39ab137a8288cb2bf5c22e8fb8b0
estebbins <stebbins.e@gmail.com>
2023-01-19 18:04:16 -0500

    Creates wine schema and model to import

--- models/wine.js
+++ models/wine.js
@@ -1,8 +1,42 @@
 /////////////////////////////////////////////////////
-//// Our schema and model for the fruit resource ////
+//// Our schema and model for the wine resource ////
 /////////////////////////////////////////////////////
 // Bring in mongoose connection from utils
 const mongoose = require('../utils/connection')
 // destructure the schema & model functions from mongoose
 const { Schema, model } = mongoose
-const commentSchema = require('./comment')
+// ! import subdoc const commentSchema = require('./comment')
+
+/////////////////////////////////////////////////////
+//// Define fruit schema & create fruit model    ////
+/////////////////////////////////////////////////////
+const wineSchema = new Schema ({
+    name: {
+        type: String
+    }, 
+    type: {
+        type: String
+    }, 
+    vineyard: {
+        type: String
+    },
+    dateTasted: {
+        type: Date
+    },
+    // !add owner after user model defined
+    // owner: {
+    //     type: Schema.Types.ObjectId,
+    //     ref: 'User'
+    // },
+    // ! subdoc / comments: [commentSchema]
+}, {
+    timestamps: true
+})
+
+const Wine = model('Wine', wineSchema)
+
+
+/////////////////////////////////////////////////////
+//// Export model                                ////
+/////////////////////////////////////////////////////
+module.exports = Wine
