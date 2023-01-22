0b32ef36e1e1545012e36b4b68e64a0848f6231f
estebbins <stebbins.e@gmail.com>
2023-01-22 11:46:23 -0500

    updates ratings controllers with liquid show routes

--- controllers/ratingControllers.js
+++ controllers/ratingControllers.js
@@ -18,6 +18,7 @@ router.post('/:wineId', (req, res) => {
     // first we get the wine ID and save to a variable
     const wineId = req.params.wineId
     // then we'll protect this route against non-logged-in users
+    // console.log('this is the session\n', req.session)
     if (req.session.loggedIn) {
         // if logged in, make the logged in user the author of the rating
         req.body.author = req.session.userId
@@ -32,15 +33,18 @@ router.post('/:wineId', (req, res) => {
                 return wine.save()
             })
             .then(wine => {
-                res.status(201).json({ wine: wine })
+                // res.status(201).json({ wine: wine })
+                res.redirect(`/wines/${wine.id}`)
             })
             // catch and handle any errors 
             .catch(err => {
                 console.log(err)
-                res.status(400).json(err)
+                // res.status(400).json(err)
+                res.redirect(`/error?error=${err}`)
             })
     } else {
-        res.sendStatus(401) // send a 401-unauthorized
+        // res.sendStatus(401) // send a 401-unauthorized
+        res.redirect(`/error?error=You%20Are%20not%20allowed%20to%20comment%20on%20this%20wine`)
     }
 
 })
@@ -62,19 +66,23 @@ router.delete('/delete/:wineId/:ratingId', (req, res) => {
                     // we can use another built in method, remove()
                     theRating.remove()
                     wine.save()
-                    res.sendStatus(204) //send 204 no content
+                    // res.sendStatus(204) //send 204 no content
+                    res.redirect(`/wines/${wine.id}`)
                 } else {
-                    res.sendStatus(401) // send a 401-unauthorized
+                    // res.sendStatus(401) // send a 401-unauthorized
+                    res.redirect(`/error?error=You%20Are%20not%20allowed%20to%20delete%20this%20rating`)
                 }
             } else{
-            // otherwise, send a 401 unauthorized status
-                res.sendStatus(401)
+                // otherwise, send a 401 unauthorized status
+                // res.sendStatus(401)
+                res.redirect(`/error?error=You%20Are%20not%20allowed%20to%20delete%20this%20rating`)
             }
             
         })
         .catch(err => {
             console.log(err)
-            res.status(400).json(err)
+            // res.status(400).json(err)
+            res.redirect(`/error?error=${err}`)
         })
 
 })
